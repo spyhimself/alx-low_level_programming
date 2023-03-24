675f4cc914d4e0df949c5a0a82043ccc607a671c
spyhimself <aymantarar7@gmail.com>
2023-03-24 16:27:32 -0700

    Added the main header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _isupper - checks for uppercase character */
+int _isupper(int c);
+
+/* _isdigit - checks for a digit (0 through 9) */
+int _isdigit(int c);
+
+/* mul - multiplies two integers */
+int mul(int a, int b);
+
+/* print_numbers - prints the numbers from 0 to 9 */
+void print_numbers(void);
+
+/* print_most_numbers - prints the numbers from 0 to 9, excluding 2 and 4 */
+void print_most_numbers(void);
+
+/* more_numbers - prints the numbers from 0 to 14, ten times */
+void more_numbers(void);
+
+/* print_line - draws a straight line in the terminal */
+void print_line(int n);
+
+/* print_diagonal - draws a diagonal line in the terminal */
+void print_diagonal(int n);
+
+/* print_square - draws a square in the terminal */
+void print_square(int size);
+
+/* print_triangle - draws a triangle in the terminal */
+void print_triangle(int size);
+
+/* print_number - prints an integer */
+void print_number(int n);
+
+#endif /* MAIN_H */
