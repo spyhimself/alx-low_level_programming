83596bff87e200e84f620426cc8de9300a874318
spyhimself <aymantarar7@gmail.com>
2023-03-27 19:15:58 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
  */
 void puts2(char *str)
 {
-	int len = _strlen(str);
-	int i;
+	int i = 0;
 
-	for (i = 0; i < len; i += 2)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 
 	_putchar('\n');
