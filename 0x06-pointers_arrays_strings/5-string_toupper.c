faa7c130155a6fea087d947d7124da529ac1fc17
spyhimself <aymantarar7@gmail.com>
2023-03-29 19:40:29 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,12 @@ char *string_toupper(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-    {
+	{
  	if (str[i] >= 'a' && str[i] <= 'z')
         {
             str[i] = str[i] - 32;
         }
     }
 
-    return str;
+    return (str);
 }
