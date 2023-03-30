1537a46cba45e8f9aba3530fb71754827655be7e
spyhimself <aymantarar7@gmail.com>
2023-03-30 16:32:11 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,29 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Buffer that stores the result
- * @size_r: Size of the buffer
- *
- * Return: Pointer to the result, or 0 if the result cannot be stored in r
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void rev_string(char *n)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0, i, j;
-	char *tmp, *res;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-
-	if (len1 + 2 > size_r || len2 + 2 > size_r)
-		return (0);
-
-	tmp = r;
-	res = r;
-
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--, tmp++)
+	while (*(n + i) != '\0')
 	{
-		sum = carry + (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j] - '0' : 0);
-		*tmp = (sum % 10) + '0';
-		carry = sum / 10;
+		i++;
 	}
+	i--;
 
-	if (carry)
+	for (j = 0; j < i; j++, i--)
 	{
-		if (size_r <= len1 + 1 || size_r <= len2 + 1)
-			return (0);
-		*tmp = carry + '0';
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
-	else
-	{
-		tmp--;
-	}
-
-	for (; tmp >= res; res++, tmp--)
-	{
-		*res ^= *tmp;
-		*tmp ^= *res;
-		*res ^= *tmp;
-	}
-
-	return (r);
 }
