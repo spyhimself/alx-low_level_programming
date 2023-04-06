70dbc592a44f6b3f169bf4def7a9c7efba9f002f
spyhimself <aymantarar7@gmail.com>
2023-04-05 19:44:11 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,53 +1,33 @@
 #include "main.h"
 
+int actual_sqrt_recursion(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
+ * @n: number to calculate the square root of
  *
- * Return: the natural square root of n,
- * or -1 if n does not have a natural square root
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0) /* error case */
-	{
+	if (n < 0)
 		return (-1);
-	}
-	if (n == 0 || n == 1) /* base case */
-	{
-		return (n);
-	}
-	return (helper(n, 1, n)); /* recursive case */
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
- * helper - finds the natural square root of a number using binary search
- * @n: the number to find the square root of
- * @start: the starting index for the search
- * @end: the ending index for the search
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
  *
- * Return: the natural square root of n,
- * or -1 if n does not have a natural square root
+ * Return: the resulting square root
  */
-int helper(int n, int start, int end)
+int actual_sqrt_recursion(int n, int i)
 {
-	int mid;
-
-	if (end >= start)
-	{
-		mid = (start + end) / 2;
-		if (mid * mid == n) /* found the natural square root */
-		{
-			return (mid);
-		}
-		else if (mid * mid > n) /* search in the left half */
-		{
-			return (helper(n, start, mid - 1));
-		}
-		else /* search in the right half */
-		{
-			return (helper(n, mid + 1, end));
-		}
-	}
-	return (-1); /* natural square root does not exist */
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
