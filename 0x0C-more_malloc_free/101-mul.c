29e8960947f3966c1ea5ed5ff51c5b3f6a3c099f
spyhimself <aymantarar7@gmail.com>
2023-04-13 18:41:27 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -66,14 +66,11 @@ int main(int argc, char *argv[])
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -97,9 +94,7 @@ int main(int argc, char *argv[])
 	}
 	if (!a)
 		_putchar('0');
-
 	_putchar('\n');
 	free(result);
-
 	return (0);
 }
