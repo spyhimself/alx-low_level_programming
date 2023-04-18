f21a8cbbb25f3be7add031eb034abc66c01c8308
spyhimself <aymantarar7@gmail.com>
2023-04-17 18:39:37 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
-struct dog {
+/**
+ * struct dog - A dog's attributes
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
